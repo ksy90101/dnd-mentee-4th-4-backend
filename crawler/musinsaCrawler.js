6f6aad593ec104663528d882b51276dcede9d06b
SeYun <ksy90101@naver.com>
2021-02-05 17:03:20 +0900

    feat: 무신사 사이트 크롤링 기능 구현

---
+++ crawler/musinsaCrawler.js
@@ -0,0 +1,77 @@
+const puppeteer = require('puppeteer');
+
+const BASE_URL = 'https://store.musinsa.com';
+
+async function getOne(page, index) {
+  const promotion = {};
+  promotion.url =
+    BASE_URL +
+    (await page.$eval(
+      `body > div.wrap > div.right_area > div.right_contents > ul > li:nth-child(${index}) > a`,
+      (data) => data.getAttribute('href'),
+    ));
+
+  promotion.iamge = await page.$eval(
+    `body > div.wrap > div.right_area > div.right_contents > ul > li:nth-child(${index}) > a > div > img`,
+    (data) => `https:${data.getAttribute('data-original')}`,
+  );
+
+  promotion.title = await page.$eval(
+    `body > div.wrap > div.right_area > div.right_contents > ul > li:nth-child(${index}) > a > strong`,
+    (data) => data.innerText,
+  );
+
+  promotion.description = await page.$eval(
+    `body > div.wrap > div.right_area > div.right_contents > ul > li:nth-child(${index}) > a > p`,
+    (data) => data.innerText,
+  );
+
+  promotion.startAt = await page.$eval(
+    `body > div.wrap > div.right_area > div.right_contents > ul > li:nth-child(${index}) > a > span`,
+    (data) => data.innerText,
+  );
+
+  promotion.endAt = await page.$eval(
+    `body > div.wrap > div.right_area > div.right_contents > ul > li:nth-child(${index}) > a > span`,
+    (data) => data.innerText,
+  );
+
+  return JSON.stringify(promotion);
+}
+
+async function getAll(page) {
+  const promotions = [];
+
+  const $promotions = await page.$$eval(
+    'body > div.wrap > div.right_area > div.right_contents > ul > li',
+    (data) => data.length,
+  );
+
+  for (let index = 0; index < $promotions; index++) {
+    const promotion = getOne(page, index + 1);
+
+    promotions.push(promotion);
+  }
+
+  return Promise.all(promotions);
+}
+
+const musinsaCrawler = async () => {
+  let promotions = [];
+
+  try {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(`${BASE_URL}/app/plan/lists`);
+
+    promotions = await getAll(page);
+
+    await browser.close();
+  } catch (e) {
+    console.log(e);
+  }
+
+  return promotions;
+};
+
+module.exports = musinsaCrawler;
