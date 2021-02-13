4a4b73971bfa4d11d7fcb2ac9ee8b2656e7fa906
JinseongHwang <eddy5360@naver.com>
2021-02-13 16:05:38 +0900

    feat: 브랜드 전체 조회 API 개발 (#23)
    * docs: 오타 수정
    
    * fix: Express 객체 중복 생성된 부분 수정
    
    * feat: 브랜드 라우터 생성
    
    * refactor: ESLint 양식에 맞게 변경

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const app = express();
 
 const { sequelize } = require('./models');
 const promotionRouter = require('./routes/promotionRouter.js');
-const categoryRouter = require('./routes/categoryRouter');
+const categoryRouter = require('./routes/categoryRouter.js');
+const brandRouter = require('./routes/brandRouter.js');
 
 app.use('/api', promotionRouter);
 app.use('/api', categoryRouter);
+app.use('/api', brandRouter);
 
 sequelize.sync().then(() => {
   app.listen(8080);
