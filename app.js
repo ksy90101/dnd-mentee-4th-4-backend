246803ba9b87f3eed465d7562f370c416b8e534c
SeYun <ksy90101@naver.com>
2021-02-13 21:21:02 +0900

    feat: 스타일 쉐어 크롤링 기능 구현

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express');
+
 const app = express();
 
 const { sequelize } = require('./models');
 const promotionRouter = require('./routes/promotionRouter.js');
 const categoryRouter = require('./routes/categoryRouter');
 
-app.use('/api', promotionRouter);
-app.use('/api', categoryRouter);
+app.use('/api', [promotionRouter, categoryRouter]);
 
 sequelize.sync().then(() => {
   app.listen(8080);
