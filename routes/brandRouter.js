f2793f5ac285efc314532dba83f99dc235b5b77b
SeYun <ksy90101@gmail.com>
2021-02-25 22:21:51 +0900

    feat: 브랜드 이름 검색 기능 구현 (#46)

--- routes/brandRouter.js
+++ routes/brandRouter.js
@@ -9,4 +9,10 @@ router.get('/brands', async (req, res) => {
   res.status(200).send(brands);
 });
 
+router.get('/brands/search', async (req, res) => {
+  const brands = await brandService.searchByName(req.query.name);
+
+  res.status(200).send(brands);
+});
+
 module.exports = router;
--- service/brandService.js
+++ service/brandService.js
@@ -1,9 +1,21 @@
 const { Brand } = require('../models');
+const sequelize = require('sequelize');
+
+const op = sequelize.Op;
 
 const findAll = () => {
   return Brand.findAll();
 };
 
+const searchByName = (brandName) => {
+  return Brand.findAll({
+    where: {
+      name: { [op.like]: `%${brandName}%` },
+    },
+  });
+};
+
 module.exports = {
   findAll,
+  searchByName,
 };
