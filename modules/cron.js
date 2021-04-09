12b9b18de77be8500d7c82a84d1731a26bfe8c33
ksy90101 <ksy90101@gmail.com>
2021-04-09 21:27:45 +0900

    feat: 연관관계 column 추가

--- modules/cron.js
+++ modules/cron.js
@@ -3,8 +3,8 @@ const executeCrawler = require('../crawler/executeCrawler');
 
 const batch = () => {
   // 매일 04:00에 Crawling 작업을 실시한다.
-  cron.schedule('0 4 * * *', () => {
-    executeCrawler.run();
+  cron.schedule('0 4 * * *', async () => {
+    await executeCrawler.run();
   });
 };
 
